80737aa3d715b9638efe6e76ca1f0cb73c752dbd
Diego Cum <79596068+diego-cum-telus@users.noreply.github.com>
2024-05-05 13:16:38 -0600

    feat(easy): Remove Element Solution, r:53ms , m:41,1mb

---
+++ easy/27-remove-element.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i = 0;
+    while(i < nums.length){
+        if(nums[i] === val){
+            nums.splice(i, 1);
+            i--;
+        }else{
+            i++;
+        }
+    }
+    return nums.length
+};
+
+// Problem:
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int val = ...; // Value to remove
+// int[] expectedNums = [...]; // The expected answer with correct length.
+//                             // It is sorted with no values equaling val.
+
+// int k = removeElement(nums, val); // Calls your implementation
+
+// assert k == expectedNums.length;
+// sort(nums, 0, k); // Sort the first k elements of nums
+// for (int i = 0; i < actualLength; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
